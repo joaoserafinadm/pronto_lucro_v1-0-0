831400aa13a7d8e01ce1754ef739cff6e118235a
joaoserafinadm <joaoserafin.adm@gmail.com>
2024-05-14 20:23:59 -0300

    feat: income add front

--- src/components/icons/index.jsx
+++ src/components/icons/index.jsx
@@ -1,5 +1,7 @@
 import { AiOutlineLeft } from "@react-icons/all-files/ai/AiOutlineLeft";
 import { AiOutlineRight } from "@react-icons/all-files/ai/AiOutlineRight";
+import { AiOutlineUp } from "@react-icons/all-files/ai/AiOutlineUp";
+import { AiOutlineDown } from "@react-icons/all-files/ai/AiOutlineDown";
 
 
 
@@ -8,10 +10,16 @@ export default function Icons(props) {
 
     switch (props.icon) {
         case 'a-l':
-            return <AiOutlineLeft className="me-2" />
+            return <AiOutlineLeft className={props.className} />
             break;
         case 'a-r':
-            return <AiOutlineRight className="me-2" />
+            return <AiOutlineRight className={props.className} />
+            break;
+        case 'a-u':
+            return <AiOutlineUp className={props.className} />
+            break;
+        case 'a-d':
+            return <AiOutlineDown className={props.className} />
             break;
         default:
     }
--- src/incomesManagement/IncomeAddPage.jsx
+++ src/incomesManagement/IncomeAddPage.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Icons from "../components/icons";
-import { faCalendar } from "@fortawesome/free-regular-svg-icons";
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar, faClipboard, faImage } from "@fortawesome/free-regular-svg-icons";
+import { faMicrophone, faMoneyBill, faTag, faWallet } from "@fortawesome/free-solid-svg-icons";
 
 
 
@@ -30,7 +30,19 @@ export default function IncomeAddPage() {
                         <span className="fw-bold">0,00</span>
                     </div>
                     <div className="d-flex fs-3 ">
-                        <span className="me-1">BRL</span>
+                        <div class="dropdown">
+                            <span class=" dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                BRL
+                            </span>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><span className="ms-2 small text-secondary">Favoritas</span></li>
+                                <li><a class="dropdown-item" href="#">BRL</a></li>
+                                <li><a class="dropdown-item" href="#">USD</a></li>
+                                <li><a class="dropdown-item" href="#">EUR</a></li>
+                                <hr />
+                                <li><a class="dropdown-item" href="#">Outras...</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -41,6 +53,28 @@ export default function IncomeAddPage() {
 
                     <div className="card shadow">
                         <div className="card-body">
+
+                            <div className="row d-flex justify-content-between">
+                                <div className="text-center d-flex align-items-center" style={{ width: "40px" }}>
+                                    <FontAwesomeIcon icon={faMoneyBill} />
+                                </div>
+                                <div className="col d-flex">
+                                    <span class=" px-2 py-1 text-white small mx-1 rounded-pill ctm-bg-success">
+                                        À Vista
+                                    </span>
+                                    <span class=" px-2 py-1 text-white small mx-1 rounded-pill ctm-bg-primary">
+                                        À Prazo
+                                    </span>
+                                    <span class=" px-2 py-1 text-white small mx-1 rounded-pill ctm-bg-primary">
+                                        Outros...
+                                    </span>
+                                </div>
+                                <div className="text-center" style={{ width: "40px" }}>
+
+                                </div>
+                            </div>
+
+                            <hr />
                             <div className="row d-flex justify-content-between">
                                 <div className="text-center d-flex align-items-center" style={{ width: "40px" }}>
                                     <FontAwesomeIcon icon={faCalendar} />
@@ -70,16 +104,74 @@ export default function IncomeAddPage() {
                                     <FontAwesomeIcon icon={faMicrophone} />
                                 </div>
                                 <div className="col d-flex">
-                                   <input type="text" class="form-control" placeholder="Descrição" />
+                                    <input type="text" class="form-control" placeholder="Descrição" />
                                 </div>
                                 <div className="text-center" style={{ width: "40px" }}>
 
                                 </div>
                             </div>
 
+                            <hr />
+
+                            <div className="row d-flex justify-content-between">
+                                <div className="text-center d-flex align-items-center" style={{ width: "40px" }}>
+                                    <FontAwesomeIcon icon={faTag} />
+                                </div>
+                                <div className="col d-flex">
+                                    <span class=" px-2 py-1 text-white small mx-1 rounded-pill ctm-bg-warning">
+                                        Vendas online
+                                    </span>
+                                </div>
+                                <div className="text-center text-secondary" style={{ width: "40px" }}>
+                                    <Icons icon='a-r' />
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div className="row d-flex justify-content-between">
+                                <div className="text-center d-flex align-items-center" style={{ width: "40px" }}>
+                                    <FontAwesomeIcon icon={faWallet} />
+                                </div>
+                                <div className="col d-flex">
+                                    <span class=" px-2 py-1  small mx-1 rounded-pill ">
+                                        <img src="/logo-sicredi.png" className="rounded-circle me-1" height={15} alt="" /> Sicredi
+                                    </span>
+                                </div>
+                                <div className="text-center text-secondary" style={{ width: "40px" }}>
+                                    <Icons icon='a-r' />
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div className="row d-flex justify-content-between">
+                                <div className="text-center d-flex align-items-center" style={{ width: "40px" }}>
+                                    <FontAwesomeIcon icon={faImage} />
+                                </div>
+                                <div className="col d-flex">
+                                    <span class=" px-2 py-1  small mx-1 rounded-pill ">
+                                        Anexo
+                                    </span>
+                                </div>
+                                <div className="text-center text-secondary" style={{ width: "40px" }}>
+                                    <Icons icon='a-r' />
+                                </div>
+                            </div>
+
+                            <hr />
+
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="row mt-3 d-flex justify-content-between">
+
+                <div className="col-12 d-flex justify-content-center">
+                    <span className="span bold text-secondary">Mais detalhes</span>
+                </div>
+
             </div>
         </div>
     )
