c1e333d90bece620b45450aa7d5a2a13557888b0
joaoserafinadm <joaoserafin.adm@gmail.com>
2024-12-20 13:26:19 -0300

    feat: manifest updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link rel='manifest' href='/manifest.json' />
+                <link rel="manifest" href="/manifest.json" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+                    rel="stylesheet"
+                />
                 <meta name="theme-color" content="#333333" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
             </Head>
-
             <body>
                 <Main />
                 <NextScript />
             </body>
         </Html>
-    )
+    );
 }
